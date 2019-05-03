b09882c53f7e3bce415e65bc7a23e4072293e0c2
Peter Mikola <mikopet@gmail.com>
2019-05-03 18:36:04 +0200

    implementing some basic visualizing for the map

--- app/controllers/admin/static_controller.rb
+++ app/controllers/admin/static_controller.rb
@@ -3,7 +3,8 @@ module Admin
     def welcome; end
 
     def visual_map
-      @resources = Map.all
+      @boundary = Map.boundary
+      @map = Map.joins(:map_type).pluck(:x, :y, :color)
     end
   end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,16 @@
 class Map < ApplicationRecord
   belongs_to :map_type
+
+  def color
+    map_type.color
+  end
+
+  class << self
+    def boundary
+      {
+          x: Map.minimum(:x)..Map.maximum(:x),
+          y: Map.minimum(:y)..Map.maximum(:y)
+      }
+    end
+  end
 end
